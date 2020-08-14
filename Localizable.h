7bbbd399c6e774857d90ce9799d632f84421d698
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2020-08-14 19:15:17 +0300

    initial commit

---
+++ Localizable.h
@@ -0,0 +1,19 @@
+//
+//  Localizable.h
+//  Localizable
+//
+//  Created by Ferhat Abdullahoglu on 14.08.2020.
+//  Copyright © 2020 Ferhat Abdullahoglu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Localizable.
+FOUNDATION_EXPORT double LocalizableVersionNumber;
+
+//! Project version string for Localizable.
+FOUNDATION_EXPORT const unsigned char LocalizableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Localizable/PublicHeader.h>
+
+
---
+++ Localization.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ferhat Abdullahoglu on 1.08.2020.
+//
+
+import Foundation
+
+/// Apps can extend this struct to supply their own `enums` each of which
+/// conforms to the `Localizable` protocol. Then on, the keys that exist in those `enums`
+/// can be used for localized strings
+public struct Localization {
+    
+    private init() {}
+    
+}
